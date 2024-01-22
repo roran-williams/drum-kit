54c01742cffdbd0dafd7fb43fbf4234dc9e6af14
roran williams <rorannwilliams@gmail.com>
2024-01-22 14:46:07 +0300

    refactor to use jQuery

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-// Drum button event listeners
-var drumButtons = document.querySelectorAll("button.drum");
 
-drumButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        handleDrumEvent(button.classList[0]);
-    });
-});
+  // Drum button event listeners
+  $(".drum").on("click", function () {
+    handleDrumEvent($(this).attr("class").split(" ")[0]);
+  });
 
-// Keypress event listener
-document.addEventListener("keypress", function (event) {
+  // Keypress event listener
+  $(document).on("keypress", function (event) {
     handleDrumEvent(event.key);
-});
+  });
 
-// Function to handle both button click and keypress events
-function handleDrumEvent(key) {
+  // Function to handle both button click and keypress events
+  function handleDrumEvent(key) {
     soundMaker(key);
     animator(key);
-}
+  }
 
-// Function to play sound based on the key
-function soundMaker(key) {
+  // Function to play sound based on the key
+  function soundMaker(key) {
     var audioFile = "./sounds/" + key + ".mp3";
     var audio = new Audio(audioFile);
     audio.play();
-}
+  }
 
-// Function to animate the pressed button
-function animator(currentKey) {
+  // Function to animate the pressed button
+  function animator(currentKey) {
     var keys = ["w", "a", "s", "d", "j", "k", "l"];
 
-    if (keys.includes(currentKey)) {
-        var activeButton = document.querySelector("." + currentKey);
-        activeButton.classList.add("pressed");
+    if ($.inArray(currentKey, keys) !== -1) {
+      var activeButton = $("." + currentKey);
+      activeButton.addClass("pressed");
 
-        setTimeout(function () {
-            activeButton.classList.remove("pressed");
-        }, 100);
+      setTimeout(function () {
+        activeButton.removeClass("pressed");
+      }, 100);
     } else {
-        console.log("Invalid key pressed");
+      console.log("Invalid key pressed");
     }
-}
+  }
+
